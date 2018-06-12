1e7a25d90cb48eabc6bc358f173a5343dbb9895d
airvi <slava33333@mail.ru>
2018-06-12 09:32:23 -0400

    continue

--- smartex.sh
+++ smartex.sh
@@ -12,10 +12,28 @@ echo "GET:"
 grep "^GET" $b | cut -f 2,3 -d " "
 fi
 
+if [[ $b =~ [^HEAD] ]]
+then
+echo "HEAD:"
+grep "^HEAD" $b | cut -f 2,3 -d " "
+fi
+
+if [[ $b =~ [^OPTIONS] ]]
+then
+echo "OPTIONS:"
+grep "^OPTIONS" $b | cut -f 2,3 -d " "
+fi
+
 if [[ $b =~ [^POST] ]]
 then
 echo "POST:"
 grep "^POST" $b | cut -f 2,3 -d " "
+fi
+
+if [[ $b =~ [^PUT] ]]
+then
+echo "PUT:"
+grep "^PUT" $b | cut -f 2,3 -d " "
 fi
 
 rm access2.log
