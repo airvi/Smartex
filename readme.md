4e3521a1745d2cb4a59832bffc05171fe2c4c20c
airvi <slava33333@mail.ru>
2018-06-18 03:41:13 -0400

    replaced smartex-array.sh, readme.md

--- readme.md
+++ readme.md
@@ -4,3 +4,5 @@ You can see two scripts to parse log file:
 
 - **smartex.sh** was organized with if statement;
 - **smartex-array.sh** was organized with an array.
+
+Please enter the name of log file after the name of script.
--- smartex-array.sh
+++ smartex-array.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-a=access.log
+cut -f 6,9 -d " " $1 | cut -c 2- | sort | uniq -c | awk '{print $2 ": " $3 ": " $1}' > temp.log
 
-cut -f 6,9 -d " " $a | cut -c 2- | sort | uniq -c | awk '{print $2 ": " $3 ": " $1}' > access2.log
-
-b=access2.log
+b=temp.log
 
 array=(CONNECT GET HEAD OPTIONS POST PUT TRACE)
 
 for i in ${array[*]}
 do
-if [[ $b =~ [^$i] ]]
-then
-echo "$i:"
-grep "^$i" $b | cut -f 2,3 -d " "
-echo
-fi
+    if [[ $b =~ [^$i] ]]
+    then
+	echo "$i:"
+	grep "^$i" $b | cut -f 2,3 -d " "
+	echo
+    fi
 done
 
-rm access2.log
+rm temp.log
