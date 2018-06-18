eb1d7ef61728e0745eddde4a1d44ea92d5c2639c
airvi <slava33333@mail.ru>
2018-06-18 04:09:44 -0400

    replaced smartex.sh

--- smartex.sh
+++ smartex.sh
@@ -1,58 +1,56 @@
 #!/bin/bash
 
-a=access.log
+cut -f 6,9 -d " " $1 | cut -c 2- | sort | uniq -c | awk '{print $2 ": " $3 ": " $1}' > temp.log
 
-cut -f 6,9 -d " " $a | cut -c 2- | sort | uniq -c | awk '{print $2 ": " $3 ": " $1}' > access2.log
-
-b=access2.log
+b=temp.log
 
 if [[ $b =~ [^CONNECT] ]]
 then
-echo "CONNECT:"
-grep "^CONNECT" $b | cut -f 2,3 -d " "
-echo
+    echo "CONNECT:"
+    grep "^CONNECT" $b | cut -f 2,3 -d " "
+    echo
 fi
 
 if [[ $b =~ [^GET] ]]
 then
-echo "GET:"
-grep "^GET" $b | cut -f 2,3 -d " "
-echo
+    echo "GET:"
+    grep "^GET" $b | cut -f 2,3 -d " "
+    echo
 fi
 
 if [[ $b =~ [^HEAD] ]]
 then
-echo "HEAD:"
-grep "^HEAD" $b | cut -f 2,3 -d " "
-echo
+    echo "HEAD:"
+    grep "^HEAD" $b | cut -f 2,3 -d " "
+    echo
 fi
 
 if [[ $b =~ [^OPTIONS] ]]
 then
-echo "OPTIONS:"
-grep "^OPTIONS" $b | cut -f 2,3 -d " "
-echo
+    echo "OPTIONS:"
+    grep "^OPTIONS" $b | cut -f 2,3 -d " "
+    echo
 fi
 
 if [[ $b =~ [^POST] ]]
 then
-echo "POST:"
-grep "^POST" $b | cut -f 2,3 -d " "
-echo
+    echo "POST:"
+    grep "^POST" $b | cut -f 2,3 -d " "
+    echo
 fi
 
 if [[ $b =~ [^PUT] ]]
 then
-echo "PUT:"
-grep "^PUT" $b | cut -f 2,3 -d " "
-echo
+    echo "PUT:"
+    grep "^PUT" $b | cut -f 2,3 -d " "
+    echo
 fi
 
 if [[ $b =~ [^TRACE] ]]
 then
-echo "TRACE:"
-grep "^TRACE" $b | cut -f 2,3 -d " "
-echo
+    echo "TRACE:"
+    grep "^TRACE" $b | cut -f 2,3 -d " "
+    echo
 fi
 
-rm access2.log
+rm temp.log
